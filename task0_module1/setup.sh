bcc8a21bcfba12c8491792a7d382a3daab4db7f6
00venfan00 <seronts@gmail.com>
2025-07-27 23:38:54 -0500

    Fix setup.sh to install Hugo 0.80.0 in Ubuntu 18.04

--- task0_module1/setup.sh
+++ task0_module1/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Simula el entorno de producción: Ubuntu 18.04 + Hugo + Make
-
+# Simula el entorno de producción con Hugo 0.80.0 en Ubuntu 18.04
 docker run --rm -it \
   -v $(pwd):/app \
   -w /app/task0_module1 \
   ubuntu:18.04 \
   bash -c "\
     apt-get update && \
-    apt-get install -y hugo make && \
+    apt-get install -y wget make && \
+    wget https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-64bit.deb && \
+    dpkg -i hugo_0.80.0_Linux-64bit.deb && \
     make build"
